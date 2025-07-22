50cdfd7c4100f26f169a8debc6acab0baf1b0dc2
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2025-07-22 14:51:03 +0100

    test: check solve output is unique
    - Check that when re-running, that solve generates new arrays, i.e. that ts and ys are new objects and not referring to mutated data.

--- test/test_basic_api.py
+++ test/test_basic_api.py
@@ -25,11 +25,30 @@ def test_basic_api():
     p = np.array([r, k, y0])
     ys, ts = ode.solve(p, 0.4, config)
 
+    assert len(ys) == 1
+    assert len(ys[0]) == 28
+    assert len(ts) == 28
+
     for i, t in enumerate(ts):
        expect = k * y0 / (y0 + (k - y0) * np.exp(-r * t))
        err = np.abs(ys[0][i] - expect)
        assert err < 1e-6
 
+    # Check that when re-running, that solve generates new arrays, i.e. that ts
+    # and ys are new objects and not referring to mutated data.
+    ys2, ts2 = ode.solve(p, 1.0, config)
+
+    # New solve generates more data due to larger t of 1.0
+    assert len(ys2[0]) == 34
+    assert len(ts2) == 34
+
+    # Old arrays refer to original data for t = 0.4
+    assert len(ys[0]) == 28
+    assert len(ts) == 28
+
+    # Sanity check that the python objects are unique
+    assert id(ys) != id(ys2)
+    assert id(ts) != id(ts2)
 
 if __name__ == "__main__":
     test_basic_api()
