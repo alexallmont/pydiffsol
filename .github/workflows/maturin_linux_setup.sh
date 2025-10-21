6a6138f8ac4fcf33c3b9fad037d7d97a0013ce02
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2025-10-21 10:50:24 +0100

    ci: remove sudos from linux setup (#2)
    - This should be unnecessary because build agent is running with full permissions.

--- .github/workflows/maturin_linux_setup.sh
+++ .github/workflows/maturin_linux_setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 if [ -f "/etc/debian_version" ]; then
     echo Installing LLVM via apt
-    sudo apt-get update
-    sudo apt-get install -y wget lsb-release software-properties-common gnupg
+    apt-get update
+    apt-get install -y wget lsb-release software-properties-common gnupg
     wget https://apt.llvm.org/llvm.sh
     chmod +x llvm.sh
     ./llvm.sh 17
-    sudo apt-get install -y libpolly-17-dev libzstd-dev zstd libncurses-dev zlib1g-dev
+    apt-get install -y libpolly-17-dev libzstd-dev zstd libncurses-dev zlib1g-dev
     export LLVM_DIR=/usr/lib/llvm-17
     export LLVM_SYS_170_PREFIX=/usr/lib/llvm-17
 
     echo Installing SparseSuite via apt
-    sudo apt-get install -y libsuitesparse-dev
+    apt-get install -y libsuitesparse-dev
 else
     echo Installing LLVM via yum
     yum update -y
