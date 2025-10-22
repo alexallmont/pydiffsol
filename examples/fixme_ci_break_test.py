fe73bb0336694b79615110e8c5a31e0f0079bbab
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2025-10-22 13:50:48 +0100

    ci: try CI breakage with minimal imports (#2)

---
+++ examples/fixme_ci_break_test.py
@@ -0,0 +1,10 @@
+# FIXME delete before merge to main; this file is for testing CI breakage
+
+import numpy as np
+import matplotlib
+matplotlib.use("SVG") # Ensure tests can run headless and in debug
+import matplotlib.pyplot as plt
+import pydiffsol as ds
+
+def test_ci_break():
+    assert True
