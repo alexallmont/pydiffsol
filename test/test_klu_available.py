c2c8a715a0feccf636327c5b29a2b7a04f1b53f5
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2025-10-27 11:43:04 +0000

    ci: migrate to cibuildwheel (#2)
    - Work by martinjrobins to resolve linkage issues using cibuildwheel instead of maturin actions; removing need for fix_macos_wheel.sh rpath fixup.

--- test/test_klu_available.py
+++ test/test_klu_available.py
@@ -2,11 +2,11 @@ import pydiffsol as ds
 import sys
 
 def test_is_klu_available():
-    # KLU currently supported on linux and macos
+    # KLU currently supported on linux
     if sys.platform.startswith("linux"):
         assert ds.is_klu_available()
     elif sys.platform == "darwin":
-        assert ds.is_klu_available()
+        assert not ds.is_klu_available()
     elif sys.platform == "win32":
         assert not ds.is_klu_available()
     else:
