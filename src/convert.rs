f8fbae04190558adfb6917446dd0a9526853dfbb
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2025-12-15 16:14:13 +0000

    chore: f32/f64 to_pyarray support

--- src/convert.rs
+++ src/convert.rs
@@ -1,15 +1,26 @@
 // Conversion methods from diffsol matrix to Python 2D array
 
+use diffsol::Scalar;
+
 use numpy::ndarray::{ArrayView1, ArrayView2, ShapeBuilder};
 use numpy::{PyArray1, PyArray2, ToPyArray};
 use pyo3::prelude::*;
 
+// Trait for valid matrix and python scalar types (f32 and f64 currently used)
+pub trait PyCompatibleScalar: Scalar + numpy::Element {}
+impl<T> PyCompatibleScalar for T where T: Scalar + numpy::Element {}
+
+// 2D matrix to python array conversion
 pub trait MatrixToPy<'py> {
-    fn to_pyarray2(&self, py: Python<'py>) -> Bound<'py, PyArray2<f64>>;
+    type T: PyCompatibleScalar;
+
+    fn to_pyarray2(&self, py: Python<'py>) -> Bound<'py, PyArray2<Self::T>>;
 }
 
-impl<'py> MatrixToPy<'py> for nalgebra::DMatrix<f64> {
-    fn to_pyarray2(&self, py: Python<'py>) -> Bound<'py, PyArray2<f64>> {
+impl<'py, T: PyCompatibleScalar> MatrixToPy<'py> for nalgebra::DMatrix<T> {
+    type T = T;
+
+    fn to_pyarray2(&self, py: Python<'py>) -> Bound<'py, PyArray2<T>> {
         let view = unsafe {
             ArrayView2::from_shape_ptr(self.shape().strides(self.strides()), self.as_ptr())
         };
@@ -17,8 +28,10 @@ impl<'py> MatrixToPy<'py> for nalgebra::DMatrix<f64> {
     }
 }
 
-impl<'py> MatrixToPy<'py> for faer::Mat<f64> {
-    fn to_pyarray2(&self, py: Python<'py>) -> Bound<'py, PyArray2<f64>> {
+impl<'py, T: PyCompatibleScalar> MatrixToPy<'py> for faer::Mat<T> {
+    type T = T;
+
+    fn to_pyarray2(&self, py: Python<'py>) -> Bound<'py, PyArray2<T>> {
         let strides = (self.row_stride() as usize, self.col_stride() as usize);
         let view =
             unsafe { ArrayView2::from_shape_ptr(self.shape().strides(strides), self.as_ptr()) };
@@ -26,19 +39,26 @@ impl<'py> MatrixToPy<'py> for faer::Mat<f64> {
     }
 }
 
+// 1D vector to python array conversion
 pub trait VectorToPy<'py> {
-    fn to_pyarray1(&self, py: Python<'py>) -> Bound<'py, PyArray1<f64>>;
+    type T: Scalar + numpy::Element;
+
+    fn to_pyarray1(&self, py: Python<'py>) -> Bound<'py, PyArray1<Self::T>>;
 }
 
-impl<'py> VectorToPy<'py> for nalgebra::DVector<f64> {
-    fn to_pyarray1(&self, py: Python<'py>) -> Bound<'py, PyArray1<f64>> {
+impl<'py, T: PyCompatibleScalar> VectorToPy<'py> for nalgebra::DVector<T> {
+    type T = T;
+
+    fn to_pyarray1(&self, py: Python<'py>) -> Bound<'py, PyArray1<T>> {
         let view = unsafe { ArrayView1::from_shape_ptr(self.len(), self.as_ptr()) };
         view.to_pyarray(py)
     }
 }
 
-impl<'py> VectorToPy<'py> for faer::Col<f64> {
-    fn to_pyarray1(&self, py: Python<'py>) -> Bound<'py, PyArray1<f64>> {
+impl<'py, T: PyCompatibleScalar> VectorToPy<'py> for faer::Col<T> {
+    type T = T;
+
+    fn to_pyarray1(&self, py: Python<'py>) -> Bound<'py, PyArray1<T>> {
         let view = unsafe { ArrayView1::from_shape_ptr(self.nrows(), self.as_ptr()) };
         view.to_pyarray(py)
     }
