75c4bc4f1243cbdd02e9d7622cb010456cc23b4f
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2026-01-14 14:47:51 +0000

    fix: heat equation matrix type
    - Updated to work with new matrix type enums after f32 support added.
    - Also changed output image dir to match other examples.

--- examples/03_heat_equation.py
+++ examples/03_heat_equation.py
@@ -15,7 +15,7 @@ def solve():
             (0..21, 0..21): -2.0,
             (1..21, 0..20): 1.0,
         }
-        b_i { 
+        b_i {
             (0): g,
             (1:20): 0.0,
             (20): g,
@@ -29,12 +29,11 @@ def solve():
         F_i {
             D * (heat_i + b_i) / (h * h)
         }
-        out_i {  
+        out_i {
             u_i
         }
-
         """,
-        ds.nalgebra_dense_f64,
+        ds.nalgebra_dense,
     )
     params = np.array([])
     ys, ts = ode.solve(params, 0.1)
@@ -43,6 +42,6 @@ def solve():
     ax.plot(ys[:,-1], label="x")
     ax.set_xlabel("x")
     ax.set_ylabel("T")
-    fig.savefig("../docs/images/heat_equation.svg")
+    fig.savefig("docs/images/heat_equation.svg")
 
 solve()
