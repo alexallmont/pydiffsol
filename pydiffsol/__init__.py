2b0dc18dbf44ec723c370d834c4ec29bedb2dfaf
martinjrobins <martinjrobins@gmail.com>
2026-02-26 11:07:41 +0000

    revert: remove multi-step ode

--- pydiffsol/__init__.py
+++
@@ -1,9 +0,0 @@
-from .pydiffsol import *
-from .multi_step_ode import MultiStepOde, MultiStepOdeConfig
-
-__doc__ = pydiffsol.__doc__
-
-if hasattr(pydiffsol, "__all__"):
-	__all__ = list(pydiffsol.__all__) + ["MultiStepOde", "MultiStepOdeConfig"]
-else:
-	__all__ = [name for name in globals() if not name.startswith("_")]
