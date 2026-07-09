6bceae6573739b77d04ef2961c533b0aa0167aae
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2026-07-09 13:28:11 +0100

    ci: ensure xml2 .so exists (#64)
    - CI failing with `-lxml2` not being found during linkage; most likely due to libxml2-devel which adds the linker stubs.
    - Note this also removes debian conditional block as currently no CI runners use this so it's never checked.

--- .github/scripts/maturin_linux_setup.sh
+++ .github/scripts/maturin_linux_setup.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
-if [ -f "/etc/debian_version" ]; then
-    echo Installing LLVM via apt
-    apt-get update
-    apt-get install -y wget lsb-release software-properties-common gnupg
-    wget https://apt.llvm.org/llvm.sh
-    chmod +x llvm.sh
-    ./llvm.sh 21
-    apt-get install -y libpolly-21-dev libzstd-dev zstd libncurses-dev zlib1g-dev
-    export LLVM_DIR=/usr/lib/llvm-21
-    export LLVM_SYS_211_PREFIX=/usr/lib/llvm-21
 
-    echo Installing SparseSuite via apt
-    apt-get install -y libsuitesparse-dev
-else
-    echo Installing LLVM via yum
-    yum update -y
-    yum -y install llvm-devel-21.1.8 clang-devel-21.1.8 libzstd-devel zstd ncurses-devel zlib-devel libffi libffi-devel
-    export LLVM_DIR=/usr
-    export LLVM_SYS_211_PREFIX=/usr
+echo Installing LLVM via yum
+yum update -y
+yum -y install \
+    llvm-devel-21.1.8 \
+    clang-devel-21.1.8 \
+    libzstd-devel \
+    zstd \
+    ncurses-devel \
+    zlib-devel \
+    libffi \
+    libffi-devel \
+    libxml2-devel
 
-    echo Installing SparseSuite via yum
-    yum install -y suitesparse-devel
-fi
+export LLVM_DIR=/usr
+export LLVM_SYS_211_PREFIX=/usr
+
+echo Installing SparseSuite via yum
+yum install -y suitesparse-devel
